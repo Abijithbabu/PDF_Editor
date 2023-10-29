732d5d72caae20f207b9a167718b37072c5fba86
Abhijith Babu <abijithb2002@gmail.com>
2023-10-29 18:17:54 +0530

    drag and drop implemented

--- client/src/components/PDFPreview.jsx
+++ client/src/components/PDFPreview.jsx
@@ -1,18 +1,78 @@
-import React from 'react';
+import { Grid } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import { Document, Page } from 'react-pdf';
 
-function PDFPreview({ file, numPages, handleSelection, selected }) {
-  return (
-    <div className="pdf-preview">
-      {Array.from({ length: numPages }, (_, index) => (
-        <div key={index} className={index+1===selected ? "pdf-preview-selected":'pdf-preview-page'} onClick={()=>handleSelection(index+1)}>
+function PDFPreview({ file, Pages, handleSelection, selected }) {
+  const [pages, setPages] = useState(Pages)
+  const generatePages = () => {
+    return Array.from({ length: numPages }, (_, index) => (
+      <div key={index}
+        onDragStart={(e) => onDragStart(e, 't.name', index)}
+        onDrop={(e) => onDrop(e, "wip", index)}
+        draggable
+        className="draggable"
+      // style={{ backgroundColor: t.bgcolor }}     
+      >
+        <div key={index} className={index + 1 === selected ? "pdf-preview-selected" : 'pdf-preview-page'} onClick={() => handleSelection(index + 1)}>
           <Document file={file}>
             <Page pageNumber={index + 1} width={100} />
           </Document>
         </div>
-      ))}
-    </div>
+      </div>
+    ))
+  }
+
+  const onDragStart = (ev, id, index) => {
+    console.log('dragstart:', id);
+    ev.dataTransfer.setData("id", id);
+    ev.dataTransfer.setData("index", index);
+  }
+
+  const onDragOver = (ev) => {
+    ev.preventDefault();
+  }
+
+  const onDrop = (ev, cat, index) => {
+    console.log(index, 'this in')
+    let id = ev.dataTransfer.getData("id");
+    let ind = ev.dataTransfer.getData("index");
+    const index2 = parseInt(ind)
+    console.log(id, index2)
+    const arr = pages;
+
+    console.log(pages);
+    console.log(arr);
+    [arr[index], arr[index2]] = [arr[index2], arr[index]];
+    console.log(arr);
+
+    setPages([...arr])
+  }
+  useEffect(() => {
+    //  setPages(generatePages()) 
+  }, [])
+
+  useEffect(() => {
+    console.log('pages', pages)
+  }, [pages])
+
+  return (
+    <Grid container spacing={5} className="pdf-preview" onDragOver={(e) => onDragOver(e)}>
+      {pages.map((page, index) => (
+        <Grid item key={index}
+          onDragStart={(e) => onDragStart(e, 't.name', index)}
+          onDrop={(e) => onDrop(e, "wip", index)}
+          onClick={() => handleSelection(Number(page.key) + 1)}
+          draggable
+          className="draggable"
+        >
+          <div className={parseInt(page.key) + 1 == selected ? "pdf-preview-selected" : 'pdf-preview-page'} >
+            {page}
+          </div>
+        </Grid>
+      ))
+      }
+    </Grid>
   );
 }
 
-export default PDFPreview;
+export default PDFPreview; 
--- client/src/components/PDFViewer.jsx
+++ client/src/components/PDFViewer.jsx
@@ -2,35 +2,39 @@ import React, { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import PDFPreview from './PdfPreview';
 import { Grid } from '@mui/material'
+
 function PDFViewer() {
   const [uploadedFile, setUploadedFile] = useState(null);
-  const [numPages, setNumPages] = useState(1);
+  const [pages, setPages] = useState(null);
   const [selected, setSelected] = useState(1)
   const handleFileChange = (file) => {
     setUploadedFile(file);
-    // pdfjs.getDocument(file).promise.then((pdf) => {
-    //   console.log(pdf.numPages)
-    //   setNumPages(pdf.numPages);
-    // });
   };
+  const generatePages = (numPages) => {
+    return Array.from({ length: numPages }, (_, index) => (
+      <Document file={uploadedFile} key={index}>
+        <Page pageNumber={index + 1} width={100} />
+      </Document>
+    ))
+  }
   const onDocumentLoadSuccess = ({ numPages }) => {
     console.log(numPages);
-    setNumPages(numPages);
+    setPages(generatePages(numPages));
   }
   return (
     <div className="app">
-      <input type="file" accept=".pdf" onChange={(e) => handleFileChange(e.target.files[0])} />
-      <Grid container spacing={3}>
-        <Grid item>
+      {!uploadedFile && <input type="file" accept=".pdf" style={{ cursor: 'pointer', zIndex: 100, padding: 50 }} onChange={(e) => handleFileChange(e.target.files[0])}/>}
+      <Grid container spacing={2} padding={6}> 
+        <Grid item lg={6}>
+          {pages && <PDFPreview file={uploadedFile} selected={selected} handleSelection={setSelected} Pages={pages} />}
+        </Grid>
+        <Grid item lg={6}>
           {uploadedFile && (
             <Document file={uploadedFile} onLoadSuccess={onDocumentLoadSuccess}>
-              <Page size="A4" pageNumber={selected} />
+              <Page pageNumber={selected} />
             </Document>
           )}
 
-        </Grid>
-        <Grid item>
-          {uploadedFile && <PDFPreview file={uploadedFile} selected={selected} handleSelection={setSelected} numPages={numPages} />}
         </Grid>
       </Grid>
     </div>
