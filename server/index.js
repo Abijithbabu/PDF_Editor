984977ef649f71e398402062702bf08b6fff9f13
Abhijith Babu <abijithb2002@gmail.com>
2023-11-01 21:00:20 +0530

    mongoose version downgrade

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 require("dotenv").config();
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: [process.env.CLIENT_URL],
   credentials: true,
 }));
 app.use(express.json());
